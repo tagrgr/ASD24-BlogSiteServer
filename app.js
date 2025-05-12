a6298b1711273f18ab6aff2f849f160618ed34a5
Tiago de Gregori <sba24297@student.cct.ie>
2025-05-12 23:26:25 +0100

    ejs all blog posts

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const bodyParser = require("body-parser");
 const PORT = 3000;
 app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view-engine", "ejs");
+
+let blogPosts = [];
 
 app.get("/", (req, res) => {
-    res.sendFile("index.html", {root: __dirname});
+    // res.sendFile("index.html", {root: __dirname});
+    res.render("index.ejs", { blogPosts });
 });
 
 app.post('/new-blog-post', (req, res) => {
@@ -14,6 +18,13 @@ app.post('/new-blog-post', (req, res) => {
     console.log(req.body.title);
     console.log(req.body.content);
     console.log(new Date().toLocaleString());
+    blogPosts.push({
+        author: req.body.author,
+        title: req.body.title,
+        content: req.body.content,
+        datetime: new Date().toLocaleString()
+    })
+    res.redirect("/");
 });
 
 app.listen(PORT, () => {
